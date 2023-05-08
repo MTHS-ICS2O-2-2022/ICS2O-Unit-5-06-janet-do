a2b3a240db7aadb614d0e135a9730f8c52d18f2f
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-08 16:46:35 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file generates the area of a triangle
+"use strict"
+function calculate() {
+  // input
+  const base = parseInt(document.getElementById("base").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const area = base * height / 2
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+}
