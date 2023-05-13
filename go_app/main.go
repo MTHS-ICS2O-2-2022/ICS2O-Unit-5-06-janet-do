ba183e9cce1e05a5ee2adbb10923169d5706cea9
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-13 20:11:32 +0000

    sync

---
+++ go_app/main.go
@@ -0,0 +1,35 @@
+// Created by: Janet Do
+// Created on: May 2023
+//
+// This program multiplies two numbers together
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This function multiplies two numbers together
+	var number1 int
+	var number2 int
+	var counter = 0
+	var answer = 0
+
+	// input
+	fmt.Println("Enter the first number: ")
+	fmt.Scanln(&number1)
+	fmt.Println("Enter the second number: ")
+	fmt.Scanln(&number2)
+
+	// process
+	for counter < number2 {
+		answer += number1
+		counter++
+	}
+
+	// output
+	fmt.Println("The answer is:", answer)
+
+	fmt.Println("\nDone.")
+}
